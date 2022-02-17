30ecfdcd14032e392c63f4ad8e18991222286ce6
Guilherme Barbosa Rogrigues <gui_tsuna@hotmail.com>
2022-02-17 15:09:12 -0300

    @eclipze00 [Task: Add Components]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { EntrarComponent } from './entrar/entrar.component';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    EntrarComponent,
+    CadastrarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
