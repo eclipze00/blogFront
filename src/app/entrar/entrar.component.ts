6f082d45c1f202084962585d5196b9e4f4c1b102
Guilherme Barbosa Rogrigues <gui_tsuna@hotmail.com>
2022-02-22 15:47:49 -0300

    @eclipze00 [Task: Edit Component's]

--- src/app/entrar/entrar.component.ts
+++ src/app/entrar/entrar.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment.prod';
+import { UserLogin } from '../model/UserLogin';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-entrar',
@@ -7,9 +11,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EntrarComponent implements OnInit {
 
-  constructor() { }
+  userLogin: UserLogin = new UserLogin ()
 
-  ngOnInit(): void {
+  constructor(
+    private auth: AuthService,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+    window.scroll(0,0)
   }
 
+  logar() {
+    this.auth.entrar(this.userLogin).subscribe({
+      next: (resp: UserLogin) => {
+        this.userLogin = resp
+        this.router.navigate(['/inicio'])
+
+        environment.foto = this.userLogin.foto
+        environment.nome = this.userLogin.nome
+        environment.id = this.userLogin.id
+        environment.token = this.userLogin.token
+
+      },
+      error: erro => {
+        if (erro.status == 401) {
+          alert('Usuário ou senha incorretos!')
+        }
+      }
+    })
+  }
 }
