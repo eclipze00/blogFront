661d12f393cc2b4d7b83e6590e0715b616547b6e
Guilherme Barbosa Rogrigues <gui_tsuna@hotmail.com>
2022-03-03 13:54:48 -0300

    @eclipze00 [Task: Add Tema Compenent/ Add Edit/Delete]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 
 import { AppComponent } from './app.component';
@@ -11,6 +12,10 @@ import { EntrarComponent } from './entrar/entrar.component';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
 import { FooterComponent } from './footer/footer.component';
 import { InicioComponent } from './inicio/inicio.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { TemaComponent } from './tema/tema.component';
+import { TemaEditComponent } from './edit/tema-edit/tema-edit.component';
+import { TemaDeleteComponent } from './delete/tema-delete/tema-delete.component';
 
 @NgModule({
   declarations: [
@@ -19,15 +24,22 @@ import { InicioComponent } from './inicio/inicio.component';
     EntrarComponent,
     CadastrarComponent,
     FooterComponent,
-    InicioComponent
+    InicioComponent,
+    TemaComponent,
+    TemaEditComponent,
+    TemaDeleteComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    FontAwesomeModule
   ],
-  providers: [],
+  providers: [{
+    useClass: HashLocationStrategy,
+    provide: LocationStrategy
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
